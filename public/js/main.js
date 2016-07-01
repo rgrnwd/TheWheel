dafd4841b4a3de6a1c23ad283b3c7b9f7863fe18
Roni Greenwood <rgrnwd@Ronis-MacBook-Pro.local>
2016-07-01 14:02:54 +1000

    hide cheer for a minute... testing deployment

--- public/js/main.js
+++ public/js/main.js
@@ -41,7 +41,7 @@ function handleWheelStopped(e) {
 
 function handleWheelStarted() {
     speechBubble.hideSpeechBubble();
-    showCheer(true);
+    showCheer(false);
 }
 
 function saveCuisine(cuisine) {
