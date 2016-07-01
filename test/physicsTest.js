d6cfd7339c980c45ac9fc7599e242f4daa99fb8a
Roni Greenwood <rgrnwd@Ronis-MacBook-Pro.local>
2016-07-01 11:02:02 +1000

    added tests to physicsTest.js - getTotalVotes

--- test/physicsTest.js
+++ test/physicsTest.js
@@ -1,4 +1,5 @@
 var physics = require('../public/js/physics.js');
+var assert = require('chai').assert;
 
 describe('Physics', function() {
     describe('distanceBetweenPoints', function() {
@@ -7,5 +8,28 @@ describe('Physics', function() {
             // var distance = physics.distanceBetweenPoints(point, point);
             // assert.equal(0, distance);
         });
+    });
+
+
+
+    describe('getTotalVotes', function() {
+
+    	it('should have zero votes if array is empty', function(){
+    		var cuisines = [];
+    		var votes = physics.getTotalVotes(cuisines);
+    		assert.equal(0, votes);
+    	});
+
+    	it('should have one vote for array with one item and one vote', function(){
+    		var cuisines = [{'votes': 1}];
+    		var votes = physics.getTotalVotes(cuisines);
+    		assert.equal(1, votes);
+    	});
+
+    	it('should aggregate votes for of all items in array', function(){
+    		var cuisines = [{'votes': 1}, {'votes': 3}, {'votes':2}];
+    		var votes = physics.getTotalVotes(cuisines);
+    		assert.equal(6, votes);
+    	});
     });
 });
